9d18f5ab2b15f38d7567aa53afc1e0bf8b2ed466
deadjoker21 <114224319+deadjoker21@users.noreply.github.com>
2022-09-23 14:21:11 +0300

    Create funkicjas.py

---
+++ funkicjas.py
@@ -0,0 +1,34 @@
+
+
+import random
+import datetime
+
+computer_number = random.randint(1, 100)
+continue_game = True
+user_guesses = []
+start_time = datetime.datetime.now()
+
+while(continue_game):
+    user_guess = int(input('Uzmini skaitli starp 1 un 100: '))
+    user_guesses.append(user_guess)
+
+    if user_guess == computer_number:
+        print('Precīzi, tu uzminēji!')
+        continue_game = False
+    elif user_guess < computer_number:
+        print('Vairāk')
+    elif user_guess > computer_number:
+        print('Mazāk')
+    else:
+        print('Notika kļūda!')
+print('Game over!')
+
+def calulate_avarage_diffrence():
+    sum_of_differences = 0
+    for n in user_guesses:
+        sum_of_differences += abs(n - computer_number)
+
+    return sum_of_differences/len(user_guesses)
+
+print(f'Vidējā starpība ir {calulate_avarage_diffrence()}')
+print(f'Tu spēlēji {datetime.datetime.now()-start_time} sekundes')
